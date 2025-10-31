c09585cfe86b844f1df0306b89d398df1666accd
cauaolivio <caua.olivio@gmail.com>
2025-10-31 00:23:47 -0300

    feat: Adiciona tela de boas vindas

--- app/src/main/java/br/edu/ifsp/scl/prmd/sc3039587/imfitplus/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prmd/sc3039587/imfitplus/MainActivity.kt
@@ -1,20 +1,24 @@
 package br.edu.ifsp.scl.prmd.sc3039587.imfitplus
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import br.edu.ifsp.scl.prmd.sc3039587.imfitplus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(amb.root)
+
+        setSupportActionBar(amb.toolbarIn.barraFerramentas)
+        supportActionBar?.subtitle = getString(R.string.main_subtitle)
+        amb.comecarBt.setOnClickListener {
+            startActivity(Intent(this, DadosPessoaisActivity::class.java))
         }
     }
 }
